84cff8eac441fdfdf2c6f98d85eb96ed3230541a
Gapenysh <bulathai2004@gmail.com>
2025-09-26 12:12:38 +0300

    add separate file for time methods

---
+++ project/utils/methods_for_datetime.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta, time
+
+from project.utils.logger import Logger
+
+
+def format_any_datetime(date_string, with_hour):
+    """
+    Автоматически определяет формат и преобразует в 'DD.MM.YYYY в HH:MM'
+    """
+    formats_to_try = [
+        "%Y-%m-%dT%H:%M:%S.%f",  # ISO с микросекундами
+        "%Y-%m-%dT%H:%M:%S",  # ISO без микросекунд
+        "%a, %d %b %Y %H:%M:%S %Z",  # RFC format
+        "%Y-%m-%d",  # Просто дата
+    ]
+
+    for fmt in formats_to_try:
+        try:
+            dt = datetime.strptime(date_string, fmt)
+            if with_hour:
+                return dt.strftime("%d.%m.%Y в %H:%M")
+            else:
+                return dt.strftime("%d.%m.%Y")
+        except ValueError:
+            continue
+
+    return date_string
+
+
+def time_calculate(time_start, time_end):
+    try:
+        if isinstance(time_start, datetime) and isinstance(time_end, datetime):
+            if time_start <= time_end:
+                time_diff = time_end - time_start
+            else:
+                time_diff = (time_end + timedelta(days=1)) - time_start
+        elif isinstance(time_start, str) and isinstance(time_end, str):
+            t_start = datetime.strptime(time_start, "%H:%M:%S").time()
+            t_end = datetime.strptime(time_end, "%H:%M:%S").time()
+
+            dt_start = datetime.combine(datetime.today(), t_start)
+            dt_end = datetime.combine(datetime.today(), t_end)
+
+            if dt_start <= dt_end:
+                time_diff = dt_end - dt_start
+            else:
+                time_diff = (dt_end + timedelta(days=1)) - dt_start
+
+        elif isinstance(time_start, time) and isinstance(time_end, time):
+            dt_start = datetime.combine(datetime.today(), time_start)
+            dt_end = datetime.combine(datetime.today(), time_end)
+
+            if dt_start <= dt_end:
+                time_diff = dt_end - dt_start
+            else:
+                time_diff = (dt_end + timedelta(days=1)) - dt_start
+        else:
+            time_diff = None
+
+        return round(time_diff.total_seconds() / 3600, 2) if time_diff else None
+    except (ValueError, TypeError, AttributeError) as e:
+        Logger.error(f"Error time calculate {str(e)}")
+        return None
