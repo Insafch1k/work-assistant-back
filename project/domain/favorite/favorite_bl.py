9e994c9b5a30c678611484d4cb4ea3ba397ea3d0
Gapenysh <bulathai2004@gmail.com>
2025-10-28 00:14:37 +0300

    add method for adding job to favorite and getting list of favorites

---
+++ project/domain/favorite/favorite_bl.py
@@ -0,0 +1,30 @@
+from project.application.entities.cities import City
+from project.application.entities.job import Job
+from project.application.entities.job_favorite import JobFavorite
+from project.domain.cities.cities_bl import CityBl
+from project.domain.favorite.favorite_dal import FavoriteJobDal
+from project.utils.data_state import DataState, DataFailedMessage, DataSuccess
+from loguru import logger
+
+class FavoriteJobBL:
+    @staticmethod
+    def add_job_to_favorite(user_id: int, job_id: int) -> DataState[JobFavorite]:
+        return FavoriteJobDal.add_job_to_favorite(user_id, job_id)
+
+    @staticmethod
+    def get_list_of_favorites(user_id: int) -> DataState[Job]:
+        jobs_state = FavoriteJobDal.get_list_of_favorites(user_id)
+        jobs = jobs_state.data
+        logger.info(f"Список избранных: {jobs}")
+        for job in jobs:
+            city_state = CityBl.get_city_name_by_id(job['city_id'])
+            if not city_state:
+                return DataFailedMessage(error_message=f"Не удалось получить информацию о городе {city_state}")
+            city_name = city_state.data
+            job['city'] = city_name
+
+        return DataSuccess(jobs)
+
+
+
+
